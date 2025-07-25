2343af40b77c34d50922cf7cf90219a8bf7bad69
eduardogomesf <eduardogoomesf@gmail.com>
2025-07-24 23:04:15 -0300

    feat: add health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Configuration } from '@/shared/configuration';
+import { HealthController } from '@/api/http';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [],
+  controllers: [HealthController],
   providers: [Configuration],
 })
 export class AppModule {}
