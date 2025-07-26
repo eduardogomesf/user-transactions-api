b3be08d5cb97a4261e84cc2cb56fc1e7fb209864
eduardogomesf <eduardogoomesf@gmail.com>
2025-07-26 10:35:18 -0300

    feat: add error codes and error message constants

---
+++ src/shared/constant/error-messages.constant.ts
@@ -0,0 +1,4 @@
+export const USER_ERROR_MESSAGES = {
+  documentInUse: 'The provided document belongs to an existing user',
+  emailInUse: 'The provided email belongs to an existing user',
+};
---
+++ src/shared/constant/index.ts
@@ -0,0 +1,2 @@
+export * from './error-codes.constant';
+export * from './error-messages.constant';
