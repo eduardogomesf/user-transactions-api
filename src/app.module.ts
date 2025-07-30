2bcb31353cbf840cfd0f8872007ba92e615c1450
eduardogomesf <eduardogoomesf@gmail.com>
2025-07-29 21:50:55 -0300

    feat: add providers and controllers to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,32 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Configuration } from '@/shared/configuration';
-import { HealthController } from '@/api/http';
+import { HealthController, UserController } from '@/api/http';
 import { UserModel } from '@/infra/database/relational/model';
+import { AddNewUserUseCase } from '@/application/use-case';
+import {
+  HashingServiceProvider,
+  IdGeneratorServiceProvider,
+  UserRepositoryProvider,
+} from '@/infra/nestjs/provider';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      inject: [Configuration],
-      useFactory: (configs: Configuration) => ({
-        type: 'postgres',
-        url: configs.relationalDb.url,
-        entities: [UserModel],
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_CONNECTION_STRING, // TO DO: use configuration instead of retrieving value from env
+      entities: [UserModel],
     }),
+    TypeOrmModule.forFeature([UserModel]), // Is calling forFeature after forRoot a good approach?
   ],
-  controllers: [HealthController],
-  providers: [Configuration],
+  controllers: [HealthController, UserController],
+  providers: [
+    Configuration,
+    AddNewUserUseCase,
+    UserRepositoryProvider,
+    HashingServiceProvider,
+    IdGeneratorServiceProvider,
+  ],
 })
 export class AppModule {}
