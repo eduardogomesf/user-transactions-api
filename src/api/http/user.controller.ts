8d0c631ffc2d2b09631c001b90cbfa36ef8fef41
eduardogomesf <eduardogoomesf@gmail.com>
2025-08-02 10:16:32 -0300

    feat: convert use case responses to http

--- src/api/http/user.controller.ts
+++ src/api/http/user.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AddNewUserDto } from './dto';
 import { AddNewUserUseCase } from '@/application/use-case';
+import { convertUseCaseResponseToHttp } from './util';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly addNewUserUseCase: AddNewUserUseCase) {}
 
   @Post()
+  @HttpCode(201)
   async create(@Body() body: AddNewUserDto) {
-    return await this.addNewUserUseCase.execute(body);
+    const useCaseResponse = await this.addNewUserUseCase.execute(body);
+    return convertUseCaseResponseToHttp(useCaseResponse);
   }
 }
