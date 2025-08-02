a467e80126266c9be56b13d3f2297c977129e6e8
eduardogomesf <eduardogoomesf@gmail.com>
2025-08-02 09:28:19 -0300

    feat: add api property decorator to AddNewUserDto

--- src/api/http/dto/add-new-user.dto.ts
+++ src/api/http/dto/add-new-user.dto.ts
@@ -1,3 +1,41 @@
-import { AddNewUserUseCaseParams } from '@/application/type';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsEmail, IsEnum } from 'class-validator';
+import { UserType } from '@/domain/entity';
 
-export class AddNewUserDto extends AddNewUserUseCaseParams {}
+export class AddNewUserDto {
+  @ApiProperty({
+    description: 'Full name of the user',
+  })
+  @IsString()
+  @IsNotEmpty()
+  full_name: string;
+
+  @ApiProperty({
+    description: `National document (aka Identity Card) of the user`,
+  })
+  @IsString()
+  @IsNotEmpty()
+  document: string;
+
+  @ApiProperty({
+    description: `Email of the user to be used for contact`,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'Password for accessing the account',
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @ApiProperty({
+    enumName: 'UserType',
+    enum: UserType,
+  })
+  @IsEnum(UserType)
+  @IsNotEmpty()
+  type: UserType;
+}
