a506dd0c19c0d4cc9d69b67270c0c1ad97336116
eduardogomesf <eduardogoomesf@gmail.com>
2025-08-09 00:42:29 -0300

    feat: add unexpected error code and message

--- src/shared/constant/error-messages.constant.ts
+++ src/shared/constant/error-messages.constant.ts
@@ -1,4 +1,12 @@
+export const ERROR_MESSAGES = {
+  unexpectedError: 'Unexpected error',
+};
+
 export const USER_ERROR_MESSAGES = {
   documentInUse: 'The provided document belongs to an existing user',
   emailInUse: 'The provided email belongs to an existing user',
+};
+
+export const CREDENTIAL_ERROR_MESSAGES = {
+  invalidCredentials: 'The provided credentials are invalid',
 };
