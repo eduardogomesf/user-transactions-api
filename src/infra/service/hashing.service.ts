5ccc5bae60856d19dc472f9c461f6caaeb1ea378
eduardogomesf <eduardogoomesf@gmail.com>
2025-08-09 21:51:40 -0300

    feat: add compare method to bcrypt hashing service

--- src/infra/service/hashing.service.ts
+++ src/infra/service/hashing.service.ts
@@ -1,4 +1,4 @@
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import { HashingService } from '@/application/type/service';
 import { Configuration } from '@/shared/configuration';
 import { Injectable } from '@nestjs/common';
@@ -9,5 +9,9 @@ export class BcryptHashingService implements HashingService {
 
   async hash(value: string): Promise<string> {
     return await hash(value, this.configs.hashing.rounds);
+  }
+
+  async compare(value: string, hashedValue: string): Promise<boolean> {
+    return await compare(value, hashedValue);
   }
 }
