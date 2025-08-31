1171622a79e7fe7e49591172af45deb51bb8486d
eduardogomesf <eduardogoomesf@gmail.com>
2025-08-31 19:07:43 -0300

    feat: add transaction entity

--- src/domain/entity/index.ts
+++ src/domain/entity/index.ts
@@ -1,3 +1,4 @@
 export * from './entity';
 export * from './user.entity';
 export * from './wallet.entity';
+export * from './transaction.entity';
---
+++ src/domain/entity/transaction.entity.ts
@@ -0,0 +1,20 @@
+import { IsNumber, IsNotEmpty, IsString, IsDate } from 'class-validator';
+import { Entity } from './entity';
+
+export class Transaction extends Entity {
+  @IsNumber()
+  @IsNotEmpty()
+  value: number;
+
+  @IsString()
+  @IsNotEmpty()
+  payeeId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  payerId: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  transferDate: Date;
+}
